3fbfc4a801c6eeec71c6f7f01c16387f06ebd49f
TracyTN <tracytn0411@gmail.com>
2019-10-10 20:13:02 -0700

    Input to add categories and subcats to db.

--- server.js
+++ server.js
@@ -11,10 +11,61 @@ var bodyParser = require('body-parser');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(cors);
+//app.use(cors);
 
 //Connect to MySQL via JawsDB
-var connection = mysql.createConnection(process.env.JAWSDB_NAVY_URL);
+// var connection = mysql.createConnection(process.env.JAWSDB_NAVY_URL);
+// connection.connect();
+
+//Use for offline testing
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'newuser1',
+  password : 'Boom',
+  database : 'effective_bookmarks'
+});
+
 connection.connect();
 
+//Routes 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+app.get('/displaycat', function(req,res) {
+  connection.query('SELECT * FROM categories', function (error, results, fields) {
+    if (error) res.send(error)
+    else res.send(results);
+  })
+})
+
+app.get('/displaySubcat', function(req, res) {
+  connection.query('SELECT * FROM subcategories', function (error, results, fileds) {
+    if (error) res.send(error)
+    else res.send(results)
+  })
+})
+
+app.post('/insertcat', function(req, res) {
+    connection.query ('INSERT INTO categories (category_name) VALUES (?)', [req.body.category_name], function(error, results, fields){
+      if (error) res.send(error)
+      else {
+          res.redirect('/')
+        }
+    });
+})
+app.post('/insertSubcat', function(req, res) {
+    connection.query ('INSERT INTO subcategories (cat_id, subcat_name) VALUES (?,?)', [req.body.cat_id, req.body.subcat_name], function(error, results, fields){
+      if (error) res.send(error)
+      else {
+          connection.query('SELECT * FROM subcategories', function (error, results, fields){
+            if (error) res.send(error)
+            else res.send(results);
+          })
+        }
+    });
+})
+
+
 app.listen(PORT, () => console.log(`listening on ${ PORT }`))
+
